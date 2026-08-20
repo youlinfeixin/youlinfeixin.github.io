244bdf9ecf6911b68501bea939b36ce38da06338
youlinfeixin <youlinfx@163.com>
2026-08-20 21:46:41 +1000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://youlinfeixin.github.io/",
+	site: "https://www.ylfx.net/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
